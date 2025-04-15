25bc353205fc9c45d13d1f108876d604d119d23e
ivan <ivandgr_16@hotmail.com>
2025-04-15 09:09:15 -0600

    feat(about): add service cards to About section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,6 +7,27 @@ import {styles} from '../styles';
 import {services} from '../constants';
 import {fadeIn, textVariant} from '../utils';
 
+const ServiceCard =({index, title, icon}) => {
+  return (
+    <Tilt className='xs:w-[250px] w-full' >
+      <motion.div 
+        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+        className='w-full green-pink-gradient p-[1px] rounded-[20px]
+      shadow-card'>
+        <div 
+          options={{max:45, scale:1, speed:450}}
+
+          className='bg-tertiary py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+        >
+          <img src={icon} alt={title} className='w-16 h-16 object-contain' />
+          <h3 className='text-white font-bold text-[20px] text-center'>{title}</h3>
+
+        </div>
+      </motion.div>
+    </Tilt>
+  )
+}
+
 const About = () => {
   return (
     // <> regular react fragment
@@ -22,6 +43,12 @@ const About = () => {
       >
         I am a Geological Engineer with a strong foundation in object-oriented programming (especially in Java) and data analysis. Passionate about technology, software development, and creating innovative, user-centered digital experiences. I have experience in tech support and customer service in both English and Spanish, which has helped me develop strong communication skills and the ability to work effectively in multicultural and remote environments.
       </motion.p>
+
+      <div className='mt-20 flex flex-wrap gap-10'>
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service}/>
+        ))}
+      </div>
     </>
   )
 }
