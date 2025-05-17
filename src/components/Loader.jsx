03ed1d20779167e1cdefc48b8ffa1c0b9bdf4cfc
ivan <ivandgr_16@hotmail.com>
2025-05-17 12:54:18 -0600

    fix(Hero): defer ComputersCanvas rendering with state to fix white screen issue on Chrome-based browsers

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,16 +1,31 @@
-import React from 'react';
-import { Html, useProgress } from '@react-three/drei';
+import { Html, useProgress } from "@react-three/drei";
 
-const Loader = () => {
+const CanvasLoader = () => {
   const { progress } = useProgress();
-
   return (
-    <Html center>
-      <div style={{ color: 'white', fontSize: '1.2rem' }}>
-        Cargando... {progress.toFixed(0)}%
-      </div>
+    <Html
+      as='div'
+      center
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
+      <span className='canvas-loader'></span>
+      <p
+        style={{
+          fontSize: 14,
+          color: "#F1F1F1",
+          fontWeight: 800,
+          marginTop: 40,
+        }}
+      >
+        {progress.toFixed(2)}%
+      </p>
     </Html>
   );
 };
 
-export default Loader;
+export default CanvasLoader;
